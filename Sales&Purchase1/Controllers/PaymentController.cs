dc1e0b69eaad62eeec4cad2ae16d32fecd494ee9
RaiT099 <88438811+RaiT099@users.noreply.github.com>
2023-06-30 18:35:00 +0800

    authorise

--- Sales&Purchase1/Controllers/PaymentController.cs
+++ Sales&Purchase1/Controllers/PaymentController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +11,7 @@ using Sales_Purchase1.Models;
 
 namespace Sales_Purchase1.Controllers
 {
+    [Authorize]
     public class PaymentController : Controller
     {
         private readonly DBContext _context;
